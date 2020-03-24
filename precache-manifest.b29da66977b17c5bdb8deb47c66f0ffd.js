432680b8c2eeb0ae6ebd87ac9315c228f2898979
kaurjasleen <jas.kaur97@gmail.com>
2020-03-24 16:23:46 +0530

    Updates

---
+++ precache-manifest.b29da66977b17c5bdb8deb47c66f0ffd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88e146550122f7937739",
+    "url": "/me.git/static/js/runtime~main.88e14655.js"
+  },
+  {
+    "revision": "5aa06dbbd9cf2ebad665",
+    "url": "/me.git/static/js/main.5aa06dbb.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me.git/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "5aa06dbbd9cf2ebad665",
+    "url": "/me.git/static/css/main.7bb4cfdb.chunk.css"
+  },
+  {
+    "revision": "567fa5ad20f164802f43ff77d40c8765",
+    "url": "/me.git/index.html"
+  }
+];
